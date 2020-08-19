7222ecac8d0be9460f5124bc4da2260618ab4dc7
Ludde86 <bslbjorn@gmail.com>
2020-08-19 20:03:45 +0200

    add todo model

---
+++ server/models/Todo.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+	todo: { type: String, required: true },
+	amount: Number,
+	isChecked: { type: Boolean, default: false },
+	date: { type: Date, default: Date.now }
+});
+
+// create and export our model
+module.exports = mongoose.model('Todo', TodoSchema);
