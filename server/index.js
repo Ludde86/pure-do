eac8af0ca3804c3ce4e70231d9c94c02237121fa
Ludde86 <bslbjorn@gmail.com>
2020-08-19 14:31:11 +0200

    add routes for get and post

--- server/index.js
+++ server/index.js
@@ -2,15 +2,47 @@
 const express = require('express');
 const app = express();
 
+// we will use this middleware, so that we can extract the data from the request object
+// this will help us to pass the request object
+app.use(express.json());
+
 app.get('/', (req, res) => {
 	res.send('Welcome to express web server');
 });
 
-app.get('/api/todo', (req, res) => {
-	res.send([ { id: 1, todo: 'clean' }, { id: 2, todo: 'landury' } ]);
+const todos = [ { id: 1, todo: 'clean' }, { id: 2, todo: 'landury' }, { id: 3, todo: 'dishes' } ];
+
+// get all todos
+app.get('/api/todos', (req, res) => {
+	res.send(todos);
 });
 
-// load environment variable (.env)
+// get a single todo
+app.get('/api/todos/:id', (req, res) => {
+	const todo = todos.find((todo) => todo.id === parseInt(req.params.id)); // req.params.id returns a String
+
+	if (!todo) {
+		res.status(404).send('The todo cannot be found');
+	}
+
+	res.send(todo);
+});
+
+// create a todo
+app.post('/api/todos', (req, res) => {
+	// we need a way to pass the request object into our todos array
+	// -> so that we can extract the data from it (request object)
+	// -> to do that, we are going to make use of a middleware
+	const todo = {
+		id: todos.length + 1,
+		todo: req.body.todo
+	};
+
+	todos.push(todo);
+	res.send(todo);
+});
+
+// setup and load environment variable (.env)
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
