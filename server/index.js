cb39d91bbeeb645b2b8da61c9ae918ba8986d181
Ludde86 <bslbjorn@gmail.com>
2020-08-19 13:39:57 +0200

    setup express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// this is how we set up a web server using the express framwork
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Welcome to express web server');
+});
+
+app.get('/api/todo', (req, res) => {
+	res.send([ { id: 1, todo: 'clean' }, { id: 2, todo: 'landury' } ]);
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
