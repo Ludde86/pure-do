b45b3da341619b26ef1b3170d3e0489c71807dc5
Ludde86 <bslbjorn@gmail.com>
2020-08-19 20:57:48 +0200

    add validation for description

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -1,31 +1,36 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const Todo = require('../models/Todo');
 const router = express.Router();
 
 // PATH: /api/todos
-router.post('/', (req, res) => {
-	// terminate this function if there is no todo
-	if (!req.body.description) {
-		return res.status(400).send({ error: 'Description is required' }); // bad request
+router.post(
+	'/',
+	[ body('description').isLength({ min: 3, max: 50 }).withMessage('Min 3 characters - Max 50 characters') ],
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(422).send({ errors: errors.array() });
+		}
+		// we need a way to pass the request object into our database
+		// -> so that we can extract the data from it (request object)
+		// -> to do that, we are going to make use of a middleware (app.use(express.json()))
+		const todo = new Todo({
+			description: req.body.description,
+			amount: req.body.amount
+		});
+
+		todo
+			.save()
+			.then((result) => {
+				res.send({
+					message: 'Todo successfully created',
+					data: result
+				});
+			})
+			.catch((err) => console.log(err));
 	}
-
-	// we need a way to pass the request object into our database
-	// -> so that we can extract the data from it (request object)
-	// -> to do that, we are going to make use of a middleware (app.use(express.json()))
-	const todo = new Todo({
-		description: req.body.description,
-		amount: req.body.amount
-	});
-
-	todo
-		.save()
-		.then((result) => {
-			res.send({
-				message: 'Todo successfully created',
-				data: result
-			});
-		})
-		.catch((err) => console.log(err));
-});
+);
 
 module.exports = router;
