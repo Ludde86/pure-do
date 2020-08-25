d7206a39c3b810f8b4580a174d8adc9e22bd900e
Ludde86 <bslbjorn@gmail.com>
2020-08-25 09:31:43 +0200

    add todo reducer

--- redux/store.js
+++ redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'react-redux';
+import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import todoReducer from './reducers/todoReducer';
