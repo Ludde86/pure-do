9f8c3bf08ea66dabdac2522434e2db2329a70142
Ludde86 <bslbjorn@gmail.com>
2020-08-26 13:35:39 +0200

    fix register route working

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 	username: {
-		typr: String,
+		type: String,
 		required: true
 	},
 	password: {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+// import the model
+const User = require('../models/User');
+
 // init the express app
 const router = express.Router();
 
 // (req, res) -> handle this request
-router.post('/register', (req, res) => {
-	res.send('register route');
+router.post('/register', async (req, res) => {
+	const user = new User({
+		username: req.body.username,
+		password: req.body.password
+	});
+
+	try {
+		const savedUser = await user.save(); // wait for the response
+		// console.log(savedUser);
+		res.send(savedUser);
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
 router.post('/login', (req, res) => {
